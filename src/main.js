f1b4e3bfc7c3bc33d9c89be3f6125eab25dc0abf
Kono Hiroki <g1121034@gmail.com>
2019-06-16 20:43:49 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,25 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import firebase from 'firebase'
+
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBaKnMM7eHUpbDAML1XWzbhoLFQq-gBhsA",
+  authDomain: "vue-firebase-7ca3d.firebaseapp.com",
+  databaseURL: "https://vue-firebase-7ca3d.firebaseio.com",
+  projectId: "vue-firebase-7ca3d",
+  storageBucket: "vue-firebase-7ca3d.appspot.com",
+  messagingSenderId: "57039567931",
+  appId: "1:57039567931:web:816e07c5f70a49ac"
+};
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+window.db = db;
+
 Vue.config.productionTip = false
 
 new Vue({
